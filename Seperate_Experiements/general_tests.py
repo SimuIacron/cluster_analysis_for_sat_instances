b98682ef647bee9cd220837e0cd39c201ab0dba2
SimuIacron <52567895+SimuIacron@users.noreply.github.com>
2021-11-11 11:23:22 +0100

    Added test for mutual info between solver_int and feature_int

---
+++ Seperate_Experiements/general_tests.py
@@ -0,0 +1,11 @@
+from DataAnalysis import scoring
+from DataFormats.DbInstance import DbInstance
+from sklearn.metrics import normalized_mutual_info_score, adjusted_mutual_info_score
+
+db_instance = DbInstance()
+
+family_int = scoring.convert_families_to_int(db_instance.family_wh)
+solver_int = scoring.get_best_solver_int(db_instance)
+
+print(adjusted_mutual_info_score(family_int, solver_int))
+
