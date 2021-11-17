Fixed Scaling error, where when using TIMESELECTBEST k=3 would always be used

Seperate_Experiements/k_means_test.py
@@ -23,6 +23,8 @@ db_instance = DbInstance()
 family_int = scoring.convert_families_to_int(db_instance.family_wh)
 solver_int = scoring.get_best_solver_int(db_instance)
 
+plot_name_1 = "009_kmeans_par2_10_best_25"
+
 fig_1 = go.Figure(layout=go.Layout(
         title=go.layout.Title(text="007_kmeans_best_scale_solver")
     ))
@@ -30,7 +32,7 @@ fig_2 = go.Figure(layout=go.Layout(
         title=go.layout.Title(text="007_kmeans_worst_scale_solver")
     ))
 fig_3 = go.Figure(layout=go.Layout(
-        title=go.layout.Title(text="007_kmeans_par2")
+        title=go.layout.Title(text=plot_name_1)
     ))
 legendCounter = -1
 for comb in output[1:]:
@@ -42,7 +44,7 @@ for comb in output[1:]:
     input_data_scaling = InputDataScaling(
         scaling_algorithm='SCALEMINUSPLUS1',
         scaling_technique='TIMESELECTBEST',
-        scaling_k_best=3
+        scaling_k_best=10
     )
 
     input_data_feature_selection = InputDataFeatureSelection(
@@ -63,7 +65,7 @@ for comb in output[1:]:
     values_1 = []
     sizes_1 = []
     # k_range = [i * 5 for i in range(1, 100)]
-    k_range = range(1, 50)
+    k_range = range(1, 25)
     # eps_range = np.arange(0.05, 2, 0.05)
     for k in k_range:
 
@@ -103,11 +105,11 @@ for comb in output[1:]:
     # fig_1.add_trace(go.Scatter(x=list(k_range), y=solver_list, mode='lines', name=" ".join(str(x) for x in comb)))
     # fig_2.add_trace(go.Scatter(x=list(k_range), y=family_list, mode='lines', name=" ".join(str(x) for x in comb)))
     fig_3.add_trace(go.Scatter(x=list(k_range), y=list_1, mode='lines', name=" ".join(str(x) for x in comb), legendgroup=legendCounter))
-    fig_3.add_trace(go.Scatter(x=keys_1, y=values_1, mode='markers', marker=dict(size=[np.sqrt(item/np.pi)*2 for item in sizes_1]), text=sizes_1, name=" ".join(str(x) for x in comb), legendgroup=legendCounter))
+    fig_3.add_trace(go.Scatter(x=keys_1, y=values_1, mode='markers', marker=dict(size=[np.sqrt(item/np.pi)*2 for item in sizes_1]), text=sizes_1, name=" ".join(str(x) for x in comb), legendgroup=legendCounter, showlegend=False))
 
 # exportFigure.export_plot_as_html(fig_1, '007_kmeans_best_scale_solver')
 # exportFigure.export_plot_as_html(fig_2, '007_kmeans_worst_scale_solver')
-exportFigure.export_plot_as_html(fig_3, '007_kmeans_par2')
+exportFigure.export_plot_as_html(fig_3, plot_name_1)
 
 # fig_1.show()
 # fig_2.show()